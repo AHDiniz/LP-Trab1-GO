9c5a4010fccb415da382c72878462532ed2e5ba6
AHDiniz <AHDiniz@github.com>
2019-04-08 19:32:31 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+/**
+ * Linguagens de Programação - Trabalho 1
+ *
+ * Alan Herculano Diniz
+ *
+ * Resolver problema de agrupamento com o algoritmo dado em aula
+ *
+ * main.go: ponto de entrada do programa
+ */
+
+package main
+
+func main() {
+	// 1 - Ler os arquivos de entrada no formato correto
+	// 2 - Criar as estruturas de dados que vão representar os dados lidos
+	// 3 - Agrupar os pontos usando o algoritmo de líder
+	// 4 - Criar e escrever nos arquivos de saída os resultados
+}
