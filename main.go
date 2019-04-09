c024f89bd0ccc94241a780d6d24cd41f8b118fb0
AHDiniz <alanhdiniz@gmail.com>
2019-04-09 11:50:13 -0300

    Testing the input reading function

--- main.go
+++ main.go
@@ -10,15 +10,17 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// 1 - Ler os arquivos de entrada no formato correto
 	// 2 - Criar as estruturas de dados que vão representar os dados lidos
 	// 3 - Agrupar os pontos usando o algoritmo de líder
 	// 4 - Criar e escrever nos arquivos de saída os resultados
 
-	// Testando a função de impressão de resultados:
-	printResults(12, [][]int{
-		[]int{1, 2},
-		[]int{3, 4, 5},
-	})
+	// Testando função de leitura de arquivo
+	pointsStr := readFile("entrada.txt") // Lendo o arquivo com os dados das coordenadas dos pontos
+	fmt.Println(readFile("entrada.txt"))
 }
